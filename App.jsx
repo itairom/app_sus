c3a1e056b8d0ef7af63df4fdab85e9ff9a4af196
itairom <iitaiirom@gmail.com>
2021-05-04 10:07:07 +0300

    first

---
+++ App.jsx
@@ -0,0 +1,27 @@
+const Router = ReactRouterDOM.HashRouter
+const { Route, Switch, Link } = ReactRouterDOM
+
+
+
+
+export function App() {
+    return (
+        <Router>
+            <header>
+            </header>
+
+            <main>
+                <Switch>
+               
+
+                </Switch>
+
+            </main>
+
+            <footer>
+                <h3>All rights reserved ©</h3>
+            </footer>
+
+        </Router>
+    )
+}
