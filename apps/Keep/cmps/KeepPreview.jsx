d011787e8c1f076c7df01831c5781ebc017633a7
gute1993-idan <igutman93@gmail.com>
2021-05-05 13:38:11 +0300

    icons

--- apps/Keep/cmps/KeepPreview.jsx
+++ apps/Keep/cmps/KeepPreview.jsx
@@ -1,21 +1,23 @@
-import { NoteTxt } from './notes/NoteTxt.jsx' 
-import { NoteImg } from './notes/NoteImg.jsx' 
-import { NoteTodos } from './notes/NoteTodos.jsx' 
+import { NoteTxt } from './notes/NoteTxt.jsx'
+import { NoteImg } from './notes/NoteImg.jsx'
+import { NoteTodos } from './notes/NoteTodos.jsx'
 import { NoteVideo } from './notes/NoteVideo.jsx'
 
-export function KeepPreview({ note, deleteNote,updateNote}) {
+export function KeepPreview({ note, deleteNote, updateNote }) {
 
-        switch (note.type) {
-            case 'NoteText':
-                return <NoteTxt note={note} deleteNote={deleteNote} updateNote={updateNote} />
-            case 'NoteImg':
-                return <NoteImg note={note} deleteNote={deleteNote}/>
-            case 'NoteTodos':
-                return <NoteTodos note={note} deleteNote={deleteNote}/>
-            case 'NoteVideo':
-                return <NoteVideo note={note} deleteNote={deleteNote}/>
-            default:
-                return <p>couldn't find note</p>
-        }
+
+    switch (note.type) {
+        case 'NoteText':
+            return <NoteTxt note={note} deleteNote={deleteNote} updateNote={updateNote} />
+        case 'NoteImg':
+            return <NoteImg note={note} deleteNote={deleteNote} />
+        case 'NoteTodos':
+            return <NoteTodos note={note} deleteNote={deleteNote} />
+        case 'NoteVideo':
+            return <NoteVideo note={note} deleteNote={deleteNote} />
+        default:
+            return <p>couldn't find note</p>
+    }
+
 
 }
--- apps/Keep/services/keep-service.js
+++ apps/Keep/services/keep-service.js
@@ -1,6 +1,10 @@
 import { utilService } from '../../../services/util-service.js'
 import { storageService } from '../../../services/storage-service.js'
 
+let gNotes;
+const KEY = 'notes'
+_createNotes()
+
 export const KeepService = {
     query,
     getNoteById,
@@ -28,7 +32,7 @@ function saveNote(note) {
 function _addNote(noteToAdd) {
     noteToAdd.id = utilService.makeId()
     gNotes.unshift(noteToAdd)
-    // _saveNotesToStorage();
+    _saveNotesToStorage();
     return Promise.resolve(noteToAdd)
 }
 
@@ -46,50 +50,68 @@ function deleteNote(noteId) {
         return noteId === note.id
     })
     gNotes.splice(noteIdx, 1)
-    // _saveNotesToStorage();
+    _saveNotesToStorage();
 
     return Promise.resolve()
 }
 
 
-var gNotes = [
-    {
-        id: utilService.makeId(),
-        type: "NoteText",
-        isPinned: true,
-        info: {
-            txt: "Fullstack Me Baby!"
-        }
-    },
-
-    {
-        id: utilService.makeId(),
-        type: "NoteImg",
-        info: {
-            url: "https://cdn.pixabay.com/photo/2016/02/22/10/06/hedgehog-1215140_960_720.jpg",
-            title: "Me playing Mi"
-        },
-        style: {
-            backgroundColor: "#00d"
-        }
-    },
-
-    {
-        id: utilService.makeId(),
-        type: "NoteTodos",
-        info: {
-            label: "ToDos",
-            todos: [
-                { txt: "Do that", doneAt: null },
-                { txt: "Do this", doneAt: 187111111 }
-            ]
-        }
-    },
-    {
-        id: utilService.makeId(),
-        type: "NoteVideo",
-        info: {
-            txt: "note-video"
-        }
+function _createNotes() {
+    var notes = storageService.loadFromStorage(KEY)
+    if (!notes || !notes.length) {
+        notes = _defaultNotes()
     }
-];
+    gNotes = notes
+    _saveNotesToStorage();
+    
+}
+
+function _saveNotesToStorage() {
+    storageService.saveToStorage(KEY, gNotes)
+}
+
+function _defaultNotes() {
+
+    return [
+        {
+            id: utilService.makeId(),
+            type: "NoteText",
+            isPinned: true,
+            info: {
+                txt: "Fullstack Me Baby!"
+            }
+        },
+
+        {
+            id: utilService.makeId(),
+            type: "NoteImg",
+            info: {
+                url: "https://cdn.pixabay.com/photo/2016/02/22/10/06/hedgehog-1215140_960_720.jpg",
+                title: "Me playing Mi"
+            },
+            style: {
+                backgroundColor: "#00d"
+            }
+        },
+
+        {
+            id: utilService.makeId(),
+            type: "NoteTodos",
+            info: {
+                label: "ToDos",
+                todos: [
+                    { txt: "Do that", doneAt: null },
+                    { txt: "Do this", doneAt: 187111111 }
+                ]
+            }
+        },
+        {
+            id: utilService.makeId(),
+            type: "NoteVideo",
+            info: {
+                txt: "note-video"
+            }
+        }
+    ];
+
+}
