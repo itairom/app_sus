e841b68346e61360cbe4fc683be64f3f67b1b989
itairom <iitaiirom@gmail.com>
2021-05-07 11:35:10 +0300

    commit

--- cmps/MainNav.jsx
+++ cmps/MainNav.jsx
@@ -5,11 +5,11 @@ const { Route, Switch, Link } = ReactRouterDOM
 export class MainNav extends React.Component {
 
     state = {
-        isAppsClicked: false
+        isAppsClicked: true
     }
 
-    toggleApps =()=>{
-
+    toggleApps = () => {
+        this.setState({ isAppsClicked: !this.state.isAppsClicked })
     }
 
 
@@ -22,10 +22,18 @@ export class MainNav extends React.Component {
         return (
             <nav className="main-nav">
                 <img src="apps/Mail/asset/img/apps.png" alt="" />
+
+
                 {isAppsClicked && <div className="apps-icon">
-                    <Link to="/mail">Mail</Link>
-                    <Link to="/keep">Keep</Link>
-                    <Link to="/">Home</Link>
+                    <Link to="/mail">
+                        <img src="assets/img/envelope.svg" />
+                    </Link>
+                    <Link to="/keep">
+                        <img src="assets/img/keep.svg" />
+                    </Link>
+                    <Link to="/">
+                        <img src="assets/img/home.svg" />
+                    </Link>
                 </div>
                 }
             </nav>
