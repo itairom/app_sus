c6bff58ae8de7cd98e50b48928426379ee957893
itairom <iitaiirom@gmail.com>
2021-05-07 09:19:32 +0300

    commit

--- apps/Mail/cmps/EmailPreview.jsx
+++ apps/Mail/cmps/EmailPreview.jsx
@@ -28,19 +28,33 @@ export class EmailPreview extends React.Component {
 
     }
 
+    dateToString(strTime) {
+        let date = new Date(strTime);
+        return date.toDateString()
+    }
+
     render() {
 
         if (!this.state.mail) return <h2>loading2</h2>
         const { mail } = this.state
-        const { subject, body, isRead, id } = mail
+        const { subject, body, isRead, id, sentAt } = mail
         return (
 
-            <div className="card-preview" >
-                <img src="apps/Mail/asset/svg/star.svg"  />
-                <div className="sender-name">Benny Gantz</div>
-             {!this.state.isClicked&&  <li className="preview-subject flex" onClick={(ev) => { this.toggleDetails(ev) }} className={(!isRead ? 'bold' : '')}  >
-                    <LongTxt className="preview-subject" text={subject} />
-                </li>}
+            <div className="card-preview " >
+                <img src="apps/Mail/asset/svg/star.svg" />
+                <div className="sender-name flex">Benny Gantz</div>
+                {!this.state.isClicked && 
+                
+                <div className="preview-subject flex" onClick={(ev) => { this.toggleDetails(ev) }} className={(!isRead ? 'bold' : '')}  >
+                    <LongTxt className="preview-subject " text={subject} />
+               
+
+
+                </div>}
+
+                <div className="msg-date flex">{this.dateToString(sentAt)}</div>
+
+
 
 
                 {this.state.isClicked && <EmailDetails toggleDetails={this.toggleDetails} onDeleteMail={this.props.onDeleteMail} mail={mail} />}
