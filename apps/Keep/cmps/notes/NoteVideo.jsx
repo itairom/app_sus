3561b3ec27cd0743b3d69e9e776287689ca86f35
itairom <iitaiirom@gmail.com>
2021-05-07 18:06:07 +0300

    commit

--- apps/Keep/cmps/notes/NoteVideo.jsx
+++ apps/Keep/cmps/notes/NoteVideo.jsx
@@ -2,10 +2,12 @@ import { NoteToolBar } from './NoteToolBar.jsx'
 export function NoteVideo({ note, deleteNote }) {
     return (
         <section className="note-video">
-            <iframe width="200" height="133" src="https://www.youtube.com/watch?v=tAe2Q_LhY8g">
-            </iframe>
-            <NoteToolBar deleteNote={deleteNote} note={note} />
-        </section>
+            {/* <iframe width="200" height="133" src="https://www.youtube.com/watch?v=tAe2Q_LhY8g">
+            </iframe> */}
+
+<iframe width="200" height="133"
+src="https://www.youtube.com/embed/tgbNymZ7vqY">
+</iframe>        </section>
 
     )
 
